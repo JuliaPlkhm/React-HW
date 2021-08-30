8cbc24eca4af61f5a9e4710bc7dddd6831430c21
j.pliakhimovich <j.pliakhimovich@gmail.com>
2021-08-30 22:15:24 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
+const meat = 'Мясо';
+const notTrue = false;
+
+function Product(props) {
+  return (
+    <div>
+      <li >{props.element}</li>
+      <button onClick={props.handleDelete(props.index)}>X</button>
+    </div>
+  )
+}
+
+
+
 
 function App() {
+  const [state, setState] = useState(["Хлеб", "Молоко", "Сыр", "Сахар"]);
+  let inputValue
+  const handleChange = (event) =>{
+    inputValue = event.target.value
+  }
+  const handleClick = (props) => {
+    console.log(props)
+
+    setState([...state, inputValue])
+  }
+  const handleDelete = (index) => () =>{
+    const filteredState = state.filter((e, i) => i !== index)
+    setState(filteredState)
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'main'>
+      <p>Список продуктов</p>
+      <ul>
+        {state.map((element, index) => (
+          <Product element={element} key={index} index = {index} handleDelete ={handleDelete}/>
+        ))}
+      </ul>
+      <input type ='text'  onChange = {handleChange}></input>
+      <button onClick={handleClick}>Add some product</button>
     </div>
   );
 }
