6a1ae19bfe9034149e1354b6a7028b66e0f1daea
j.pliakhimovich <j.pliakhimovich@gmail.com>
2021-09-02 17:28:04 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -10,21 +10,24 @@ function Product(props) {
   )
 }
 
-let inputValue
+
 
 function App() {
   const [state, setState] = useState(["Хлеб", "Молоко", "Сыр", "Сахар"]);
-  const handleChange = (event) => {
-    inputValue = event.target.value
-  }
+  const [value, setValue] = useState("");
+
+ 
   const handleClick = (props) => {
-    if (inputValue) {
-      setState([...state, inputValue])
-    }
+    setState([...state, value]);
+    setValue("")
   }
   const handleDelete = (index) => () => {
     const filteredState = state.filter((e, i) => i !== index)
     setState(filteredState)
+  }
+  const handleChange = (event) => {
+    setValue(event.target.value)
+
   }
 
   return (
@@ -35,7 +38,7 @@ function App() {
           <Product element={element} key={index} index={index} handleDelete={handleDelete} />
         ))}
       </ul>
-      <input type='text' placeholder='Введите продукт' onChange={handleChange}></input>
+      <input type='text' placeholder='Введите продукт' onChange={handleChange} value ={value}></input>
       <button onClick={handleClick}>Add some product</button>
     </div>
   );
